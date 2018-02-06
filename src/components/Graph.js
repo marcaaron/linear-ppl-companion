8d24c8f9bb79ee6793eb5e363a25000a84f580c7
Marc Aaron <marc.aaron.glasser@gmail.com>
2018-02-05 22:11:03 -0800

    Enable Chart.js and map color codes to line graph

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Chart from 'chart.js';
 
 class Graph extends Component {
   	render() {
@@ -6,55 +7,43 @@ class Graph extends Component {
 			dates = dates.map((item)=>(item.date).substring(0,item.date.lastIndexOf('/')));
 		let weights = this.props.workouts;
 			weights = weights.map((item)=>item.workout[0].weight);
-
-		let marks = [];
-		for(let z=0;z<dates.length;z++){
-			const string = [dates[z],weights[z]].toString();
-			marks.push(string);
-		}
-		weights = weights.reverse();
-		const dateStyle = {gridRow:dates.length+1};
-		const buttStyle = {gridRow:dates.length+1};
-		let templateString = '';
-		for(let i=0; i<dates.length;i++){templateString+='auto '};
-		const templateStyle = {gridTemplateColumns: templateString};
-		const gridSpaceStyle = {gridTemplateColumns: templateString, gridColumn: `2/${dates.length+2}`, gridRow: `1/${dates.length+1}`};
-		let space = [];
-		for(let i=0; i<dates.length;i++){
-			for(let j=0; j<dates.length; j++){
-				space.push([dates[j], weights[i]]);
+		let colorBox = document.querySelector('.table-exercise');
+		let color = getComputedStyle(colorBox).backgroundColor;
+			console.log(color);
+			if(document.getElementById("myChart")){
+				var ctx = document.getElementById("myChart").getContext('2d');
+				var myChart = new Chart(ctx, {
+				    type: 'line',
+				    data: {
+				        labels: dates,
+				        datasets: [{
+				            label: 'Weight in Lbs',
+				            data: weights,
+				            backgroundColor: [
+				                color
+				            ],
+				            borderColor: [
+				                'rgba(255,99,132,1)'
+				            ],
+				            borderWidth: 1
+				        }]
+				    },
+				    options: {
+						maintainAspectRatio: false,
+				        scales: {
+				            yAxes: [{
+				                ticks: {
+				                    beginAtZero:false
+				                }
+				            }]
+				        }
+				    }
+				});
 			}
-		}
+
 		return (
-			<div style={templateStyle} className="graph-container">
-				<div style={buttStyle} className="grid-item"></div>
-				{
-					dates.map((date)=>{
-						return[
-							<div style={dateStyle} className="grid-item date-tick">{date}</div>
-						]
-					})
-				}
-
-				<div style={gridSpaceStyle} className="grid-space">
-					{
-						space.map((grid,index)=>{
-							return[
-								<div className="grid-space-items">
-								{marks.includes(grid.toString())?'•': ' '}
-								</div>
-							]
-						})
-					}
-				</div>
-
-				{
-					weights.map((weight)=>{
-						return[
-							<div className="grid-item weight-tick">{weight}</div>
-						]
-					})
-				}
+			<div className ='chart-container'>
+				<canvas id="myChart"></canvas>
 			</div>
 		);
   	}
